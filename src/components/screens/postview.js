2bf500e25ca894d86aa4b09631a4e30ae0bcdac1
shyam kumar <shyamnits98@gmail.com>
2022-07-27 10:37:59 +0530

    make it better

--- src/components/screens/postview.js
+++ src/components/screens/postview.js
@@ -1,44 +1,44 @@
-import React,{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './post-view.css'
-const PostView = ()=> {
-    const[data,setData] = useState([])
-         useEffect(()=>{
-            fetch('/allpost',{
-                headers:{
-                    
-                }
-            }).then(res=>res.json())
-             .then(result=>{
-            setData(result.posts)
-             })
-            
-         },[])
+const PostView = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('/allpost', {
+            headers: {
 
-  return (
-    
-        <div className='home'>
-          
-          {
-            data.map(item=>{
-         return(        
-        <div className='card home-card'>
-        <h4>{item.author}</h4>
-        <div className='card-image'>
-            <img src={item.photo} alt='randon' /> 
-         
-        </div>
-        <div className='card-content'>
-        <i className="material-icons" style={{color:"red"}}>favorite</i>
-            <h6>{item.location}</h6>
-            <p>{item.description} </p>
-            <input type="text" placeholder="add a comment"/>
-        </div>
-    </div>
-
-             )
+            }
+        }).then(res => res.json())
+            .then(result => {
+                setData(result.posts)
             })
-          }
-      </div>
-  )
+
+    }, [])
+
+    return (
+
+        <div className='home'>
+
+            {
+                data.map(item => {
+                    return (
+                        <div className='card home-card'>
+                            <h5>{item.title}</h5>
+                            <div className='card-image'>
+                                <img src={item.photo} alt='randon' />
+
+                            </div>
+                            <div className='card-content'>
+                                <i className="material-icons" style={{ color: "red" }}>favorite</i>
+                                <h6>{item.body}</h6>
+                                <p>{item.description} </p>
+                                <input type="text" placeholder="add a comment" />
+                            </div>
+                        </div>
+
+                    )
+                })
+            }
+        </div>
+    )
 }
 export default PostView;
