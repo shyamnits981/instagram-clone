864760601820858783697be999e80a18f406f82e
shyam kumar <shyamnits98@gmail.com>
2022-07-27 23:41:52 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/Navbar';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './components/screens/Home';
 import Profile from './components/screens/Profile';
-import SignIn from './components/screens/SignIn';
+// import SignIn from './components/screens/SignIn';
 import Signup from './components/screens/Signup';
 import CreatePost from './components/screens/CreatePost';
 import LandingPage from './components/screens/landing-page';
@@ -18,7 +18,7 @@ function App() {
     <Route path="/" element={ <Home/> } />
     <Route path="/landing-page" element={ <LandingPage/> }/>
     <Route path="/postview" element={ <PostView/> }/> 
-    <Route path="/signin" element={ <SignIn/> } />
+    {/* <Route path="/signin" element={ <SignIn/> } /> */}
     <Route path="/signup" element={ <Signup/> } />
     <Route path="/profile" element={ <Profile/> } />
     <Route path="/createpost" element={ <CreatePost/> } />
